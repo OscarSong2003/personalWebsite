906d5dfd65939307d2e412849462a1f95b6812d6
osong <Oscar.Song03@gmail.com>
2023-01-03 20:57:31 -0500

    added incoming internship

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -14,6 +14,13 @@ const Experience = (): React.ReactElement => {
              <Heading mb={5} color="whiteAlpha.900" className="animate-character3">Experience</Heading>
              <Flex mt={9} direction="column" justifyContent="left" alignItems="left"
              width="50%" marginLeft="2px" borderLeft="2px" borderLeftWidth="2px" pl={5} borderColor="whiteAlpha.700">
+                <Text fontSize="22px" color="whiteAlpha.800">Incoming Software Development Engineer Intern @ Amazon</Text>
+                <Text mt={1} fontSize="20px" color="whiteAlpha.800">San Diego, California</Text>
+                <Text mt={1} fontSize="20px" color="whiteAlpha.800">September 2022 - Present</Text>
+            </Flex>
+
+             <Flex mt={9} direction="column" justifyContent="left" alignItems="left"
+             width="50%" marginLeft="2px" borderLeft="2px" borderLeftWidth="2px" pl={5} borderColor="whiteAlpha.700">
                 <Text fontSize="22px" color="whiteAlpha.800">Research Intern @ Michigan Human-AI Lab</Text>
                 <Text mt={1} fontSize="20px" color="whiteAlpha.800">Ann Arbor, Michigan</Text>
                 <Text mt={1} fontSize="20px" color="whiteAlpha.800">May 2022 - Present</Text>
@@ -28,16 +35,9 @@ const Experience = (): React.ReactElement => {
                     user interface using React, Django, Forte, and Stave. Learn more in my projects section!
                 </Text>
              </Flex>
-             {/* <Box mt={9} width="20%" justifyContent="center" alignItems="center">
-                <hr></hr>
-             </Box> */}
-             {/* <Flex direction="column" justifyContent="left" alignItems="left"
-             width="50%">
-                <Text mt={8} fontSize="22px" color="whiteAlpha.800">Student Developer @ The Friday Projects</Text>
-                <Text mt={1} fontSize="20px" color="whiteAlpha.800">Remote</Text>
-                <Text mt={1} fontSize="20px" color="whiteAlpha.800">Summer 2021 - Present</Text>
-
-                <Text my={5} fontSize="20px" color="whiteAlpha.800">
+             {/* Incoming */}
+             
+                {/* <Text my={5} fontSize="20px" color="whiteAlpha.800">
                     Enhanced the Human-Computer Interaction of machine learning toolkits 
                     for the CASL Project by collaborating with Professor Anhong Guo and Hector Liu, 
                     Head of Engineering at Petuum Inc.</Text>
@@ -45,11 +45,8 @@ const Experience = (): React.ReactElement => {
                 <Text fontSize="20px" color="whiteAlpha.800">
                     Developed an audio processing application along with a natural language processing graphical
                     user interface using React, Django, Forte, and Stave. Learn more in my projects section!
-                </Text>
-             </Flex>
-             <Box mt={9} width="20%" justifyContent="center" alignItems="center">
-                <hr></hr>
-             </Box> */}
+                </Text> */}
+            
         </Flex>
     )
 }
